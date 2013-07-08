6f6c0180f58d860e9bc30febbe59339aa2f4f14a
Jon Waghorn <jon.waghorn@gmail.com>
2013-07-08 14:06:51 +1200

    Fix spec predicates. Test alert-error case.

--- spec/helpers/bootstrap_flash_helper_spec.rb
+++ spec/helpers/bootstrap_flash_helper_spec.rb
@@ -8,22 +8,27 @@ describe BootstrapFlashHelper do
        bootstrap_flash.should == "<div class=\"alert alert-warning\"><a class=\"close\" data-dismiss=\"alert\">&times;</a>Update Warning!</div>"
      end
 
-     it "should return alert-success message when use notice message" do
+     it "should return alert-success message when using notice message" do
        stub!(:flash).and_return({:notice => "Update Success!"})
        bootstrap_flash.should == "<div class=\"alert alert-success\"><a class=\"close\" data-dismiss=\"alert\">&times;</a>Update Success!</div>"
      end
 
-     it "should return flash message" do
+     it "should return alert-error message when using notice error" do
        stub!(:flash).and_return({:error => "Update Failed!"})
        bootstrap_flash.should == "<div class=\"alert alert-error\"><a class=\"close\" data-dismiss=\"alert\">&times;</a>Update Failed!</div>"
      end
 
-     it "should return alert-success message when use notice message" do
+     it "should return alert-error message when using alert message" do
+       stub!(:flash).and_return({:alert => "Update Alert!"})
+       bootstrap_flash.should == "<div class=\"alert alert-error\"><a class=\"close\" data-dismiss=\"alert\">&times;</a>Update Alert!</div>"
+     end
+
+     it "should return alert-info message when using info message" do
        stub!(:flash).and_return({:info => "Update Information!"})
        bootstrap_flash.should == "<div class=\"alert alert-info\"><a class=\"close\" data-dismiss=\"alert\">&times;</a>Update Information!</div>"
      end
 
-     it "should return alert-success message when use notice message" do
+     it "should return no message when using an undefined message" do
        stub!(:flash).and_return({:undefined => "Update Undefined!"})
        bootstrap_flash.should == ""
      end
